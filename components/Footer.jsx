c716f0e2ff47ec2bcecb75fa22d15936bc9faa9b
LeviNoppers <levinoppers69@gmail.com>
2024-09-27 21:30:29 +0100

    added font

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer style={{position: 'relative', bottom: '0', left: '1em'}}>
-      <p>2024 Something. All rights reserved.</p>
+    <footer style={{position: 'relative', bottom: '0', left: '1em', fontFamily: 'poppins'}}>
+      <p>2024, Something.</p>
     </footer>
   );
 }
