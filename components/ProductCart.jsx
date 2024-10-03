85168cfb3993f7d3add7bdfe0b088960251c5892
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-10-03 14:03:48 +0200

    added deletion of cart

--- components/ProductCart.jsx
+++ components/ProductCart.jsx
@@ -2,15 +2,42 @@ import React from "react";
 import styles from "../styles/components/ProductCart.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import GlobalContext from '../global/GlobalContext';
 
 export default function ProductCart({ item }) {
   const router = useRouter();
+  const { products, setCart } = React.useContext(GlobalContext);
+
+  const removeFromCart = async(id) => {
+    const user_id = 1;
+
+    try {
+      const response = await fetch('/api/cart', {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          product_id: id,
+          user_id: user_id
+        })
+      });
+      
+      if (!response.ok) {
+        throw new Error('Failed to remove from cart');
+      }
+      location.reload()
+    } catch (error) {
+      console.error("Error removing item from cart:", error);
+    }
+  };
+  
+
   return (
     <div className={styles.ProductCart}>
         <div
           key={item.id}
           className={styles.productCart}
-          onClick={() => router.push(item.url)}
         >
           <Image
             priority
@@ -22,8 +49,8 @@ export default function ProductCart({ item }) {
           <div>
             <h2>{item.name}</h2>
             <div className={styles.buttons}>
-              <label>${item.price}</label>
-              <label><Image src={'/icons/shoppingcart_remove.png'} width={25} height={25} alt="remove"/></label>
+              <label onClick={() => router.push(item.url)}>${item.price}</label>
+              <label onClick={() => removeFromCart(item.id)}><Image src={'/icons/shoppingcart_remove.png'} width={25} height={25} alt="remove"/></label>
             </div>
           </div>
         </div>
