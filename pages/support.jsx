83553722d1b92630e547cc7726b27cbc742db1f7
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-10-04 19:52:03 +0200

    simple start for support.jsx

--- pages/support.jsx
+++ pages/support.jsx
@@ -1,5 +1,17 @@
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import styles from '../styles/Support.module.css';
+
 const Support = () => {
-  
+  return(
+    <>
+    <Nav/>
+      <main className={styles.main}>
+        
+      </main>
+    <Footer/>
+    </>
+  )
 }
 
 export default Support;
