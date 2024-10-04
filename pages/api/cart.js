0cfd050b13c0114a7255eca78608ac63b74b0a69
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-10-04 18:58:52 +0200

    formatted

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -18,38 +18,40 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ cart });
   } else if (req.method === "POST") {
-      const { product_id, user_id } = req.body;
-      const { data: existingCart, error: fetchError } = await supabase
-        .from("cart")
-        .select("*")
-        .eq("user_id", user_id)
-        .single();
+    const { product_id, user_id } = req.body;
+    const { data: existingCart, error: fetchError } = await supabase
+      .from("cart")
+      .select("*")
+      .eq("user_id", user_id)
+      .single();
 
-      if (fetchError) {
-        console.error("Error fetching cart:", fetchError);
-        return res.status(500).json({ error: "Failed to fetch user's cart" });
+    if (fetchError) {
+      console.error("Error fetching cart:", fetchError);
+      return res.status(500).json({ error: "Failed to fetch user's cart" });
+    }
+
+    let updatedProductIds = [product_id];
+    if (existingCart) {
+      const currentProductIds = existingCart.product_id.split(",");
+      if (!currentProductIds.includes(product_id)) {
+        updatedProductIds = [...currentProductIds, product_id];
       }
+    }
+    const updatedProductIdsString = updatedProductIds.join(",");
 
-      let updatedProductIds = [product_id];
-      if (existingCart) {
-        const currentProductIds = existingCart.product_id.split(',');
-        if (!currentProductIds.includes(product_id)) {
-          updatedProductIds = [...currentProductIds, product_id];
-        }
-      }
-      const updatedProductIdsString = updatedProductIds.join(',');
+    const { data, error } = await supabase
+      .from("cart")
+      .upsert([{ product_id: updatedProductIdsString, user_id }], {
+        onConflict: ["user_id"],
+      });
 
-      const { data, error } = await supabase
-        .from("cart")
-        .upsert([{ product_id: updatedProductIdsString, user_id }], { onConflict: ['user_id'] });
+    if (error) {
+      console.error("Error updating cart:", error);
+      return res.status(500).json({ error: "Failed to update cart" });
+    }
 
-      if (error) {
-        console.error("Error updating cart:", error);
-        return res.status(500).json({ error: "Failed to update cart" });
-      }
-
-      return res.status(200).json({ message: "Cart updated successfully" });
-  } else if(req.method === 'DELETE') {
+    return res.status(200).json({ message: "Cart updated successfully" });
+  } else if (req.method === "DELETE") {
     const { product_id, user_id } = req.body;
     const { data: existingCart, error: fetchError } = await supabase
       .from("cart")
@@ -68,7 +70,7 @@ export default async function handler(req, res) {
 
     let productIdsArray = existingCart.product_id.split(",");
     const indexToRemove = productIdsArray.indexOf(product_id.toString());
-    
+
     if (indexToRemove !== -1) {
       productIdsArray.splice(indexToRemove, 1);
     }
@@ -85,7 +87,9 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: "Failed to update cart" });
     }
 
-    return res.status(200).json({ message: "Product removed from cart successfully", data });
+    return res
+      .status(200)
+      .json({ message: "Product removed from cart successfully", data });
   } else {
     res.setHeader("Allow", ["GET", "POST"]);
     res.status(405).end(`Method ${req.method} not allowed`);
