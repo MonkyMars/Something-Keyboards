99b7a526bb28236e23e49f432c3166ffa01ea090
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-10-07 19:17:31 +0200

    added logging in

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,34 +1,52 @@
 import { supabase } from "../../lib/supabaseClient";
+import bcrypt from "bcrypt";
 
 export default async function user(req, res) {
   if (req.method === "POST") {
     try {
-      const { email, password } = req.body;
+      const { email, password, first_name, last_name, display_mode } = req.body;
 
       const { data: existingUser, error: fetchError } = await supabase
         .from("users")
         .select("*")
-        .eq("email", email);
+        .ilike("email", email);
 
       if (fetchError) {
         return res.status(500).json({ error: "Error fetching user data", fetchError });
       }
 
-      if (existingUser.length > 0) {
-        return res.status(400).json({ error: "User already exists" });
+      if (first_name) {
+        if (existingUser.length > 0) {
+          return res.status(400).json({ error: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const { error: insertError } = await supabase
+          .from("users")
+          .insert([{ email, password: hashedPassword, first_name, last_name, display_mode }]);
+
+        if (insertError) {
+          return res.status(500).json({ error: "Failed to add user to public.users", insertError });
+        }
+
+        return res.status(200).json({ message: "User successfully created" });
+      } else {
+        console.log(existingUser)
+        if (existingUser.length > 0) {
+          const validPassword = await bcrypt.compare(password, existingUser[0].password);
+
+          if (validPassword) {
+            return res.status(200).json({ message: "Login successful" });
+          } else {
+            return res.status(401).json({ error: "Invalid credentials" });
+          }
+        } else {
+          return res.status(404).json({ error: "User not found" });
+        }
       }
-
-      const { error: insertError } = await supabase
-        .from("users")
-        .insert([{ email, password }]);
-
-      if (insertError) {
-        return res.status(500).json({ error: "Failed to add user to public.users", insertError });
-      }
-
-      return res.status(200).json({ message: "User successfully created" });
     } catch (error) {
-      return res.status(500).json({ error: "Failed to add user", details: error.message });
+      return res.status(500).json({ error: "Internal server error", details: error.message });
     }
   } else {
     res.setHeader("Allow", ["POST"]);
--- pages/user/login.jsx
+++ pages/user/login.jsx
@@ -3,12 +3,17 @@ import styles from "../../styles/user/Login.module.css";
 import React from "react";
 import GlobalContext from "../../global/GlobalContext";
 import { useRouter } from "next/router";
+
 export default function Login() {
   const router = useRouter();
   const { user, setUser } = React.useContext(GlobalContext);
+  const [formPage, setFormPage] = React.useState(0);
   const [formData, setFormData] = React.useState({
     email: "",
     password: "",
+    first_name: "",
+    last_name: "",
+    display_mode: 0,
   });
 
   React.useEffect(() => {
@@ -30,56 +35,140 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("/api/user", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await response.json();
-      console.log(data)
+      if (formPage === 0) {
+        const response = await fetch("/api/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        });
+
+        if (response.ok) {
+          setUser(formData);
+          router.push("/user/account");
+        } else {
+          window.alert("Login failed");
+        }
+      } else if (formPage === 2 && formData) {
+        const response = await fetch("/api/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          setUser(formData);
+          router.push("/user/account");
+        } else {
+          window.alert("Sign up failed");
+        }
+      } else if(formPage === 1){
+        if(formData.email && formData.password) {
+          setFormPage(2)
+        } e
+      }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
-  
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const toggleDisplayMode = (mode) => {
-    setUser((prevUser) => ({ ...prevUser, display_mode: mode }));
+  const toggleDisplayMode = () => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      display_mode: user.display_mode === 1 ? 0 : 1,
+    }));
   };
+
   return (
     <>
       <nav className={styles.Nav}>
-        <h2 onClick={() => router.push('/')}>{"Something"}</h2>
-        <div onClick={() => toggleDisplayMode(user.display_mode === 1 ? 0 : 1)}>
-          {user.display_mode}
+        <h2 onClick={() => router.push("/")}>{"Something"}</h2>
+        <div onClick={toggleDisplayMode}>
+          {user.display_mode === 1 ? "Dark" : "Light"}
         </div>
       </nav>
       <main className={styles.mainContainer}>
         <form onSubmit={handleSubmit}>
-          <h2>{"Login"}</h2>
-          <label>{"Email Address:"}</label>
-          <input
-            type="email"
-            placeholder="..."
-            value={formData.email}
-            name="email"
-            onChange={handleInputChange}
-          />
-          <label>{"Password:"}</label>
-          <input
-            type="password"
-            placeholder="..."
-            value={formData.password}
-            name="password"
-            onChange={handleInputChange}
-          />
-          <button type="submit">{"Submit"}</button>
+          {formPage === 0 && (
+            <>
+              <h2>{"Login"}</h2>
+              <label>{"Email Address:"}</label>
+              <input
+                type="email"
+                placeholder="..."
+                value={formData.email}
+                name="email"
+                onChange={handleInputChange}
+              />
+              <label>{"Password:"}</label>
+              <input
+                type="password"
+                placeholder="..."
+                value={formData.password}
+                name="password"
+                onChange={handleInputChange}
+              />
+            </>
+          )}
+          {formPage === 1 && (
+            <>
+              <h2>{"Sign up"}</h2>
+              <label>{"Email Address:"}</label>
+              <input
+                type="email"
+                placeholder="..."
+                value={formData.email}
+                name="email"
+                onChange={handleInputChange}
+              />
+              <label>{"Password:"}</label>
+              <input
+                type="password"
+                placeholder="..."
+                value={formData.password}
+                name="password"
+                onChange={handleInputChange}
+              />
+            </>
+          )}
+          {formPage === 2 && (
+            <>
+              <h2>{"Sign up"}</h2>
+              <label>{"First Name:"}</label>
+              <input
+                type="text"
+                placeholder="..."
+                value={formData.first_name}
+                name="first_name"
+                onChange={handleInputChange}
+              />
+              <label>{"Last Name:"}</label>
+              <input
+                type="text"
+                placeholder="..."
+                value={formData.last_name}
+                name="last_name"
+                onChange={handleInputChange}
+              />
+            </>
+          )}
+          <button type="submit">{formPage === 1 ? 'Continue' :"Submit"}</button>
+          <label onClick={() => setFormPage(formPage === 0 ? 1 : 0)}>
+            {formPage === 0
+              ? "Don't have an account? Create one!"
+              : "Already have an account? Login!"}
+          </label>
         </form>
       </main>
       <Footer />
