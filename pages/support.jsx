ef1e0103fec5edb8a004defe8e94f48ec72e115c
LeviNoppers <155177968+MonkyMars@users.noreply.github.com>
2024-10-16 13:24:23 +0000

    working on support page (not done)

--- pages/support.jsx
+++ pages/support.jsx
@@ -1,20 +1,67 @@
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-import styles from '../styles/Support.module.css';
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import styles from "../styles/Support.module.css";
+import GlobalContext from ".//../global/GlobalContext";
+import Image from "next/image";
+import React from "react";
 
 const Support = () => {
-  return(
+  const { user } = React.useContext(GlobalContext);
+  const [page, setPage] = React.useState(0);
+  const support_options = [
+    {
+      name: "Site Bugs",
+      icon:
+        user.display_mode === 0
+          ? "/icons/bugreport.png"
+          : "/icons/lightmode/bugreport_white.png",
+    },
+    {
+      name: "Products",
+      icon:
+        user.display_mode === 0
+          ? "/icons/inventory.png"
+          : "/icons/lightmode/inventory_white.png",
+    },
+  ];
+
+  return (
     <>
-    <Nav/>
+      <Nav />
       <main className={styles.main}>
-        
-        <div>
-        <h2>{'Support'}</h2>
-        </div>
+        <aside className={styles.supportOptions}>
+          <h2>{"Support"}</h2>
+          <div>
+            {support_options?.map((option, index) => (
+              <label
+                key={index}
+                onClick={() => setPage(index)}
+                style={{
+                  outline:
+                    page === index ? "2.5px solid var(--button-color)" : "none",
+                }}
+              >
+                <Image
+                  src={option.icon}
+                  alt={option.name}
+                  width={25}
+                  height={25}
+                />{" "}
+                {option.name}
+              </label>
+            ))}
+          </div>
+        </aside>
+        <section className={styles.section}>
+          <h2>{support_options[page].name}</h2>
+          {page === 0 && <>
+            <button>Report Error</button>Report Visual Bug<button></button><button></button>
+          </>}
+        </section>
       </main>
-    <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Support;
