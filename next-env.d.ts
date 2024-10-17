6eeaa22b41e3fe39bf0fdc469959ec1768be9fd4
Levi Noppers <=>
2024-10-17 19:16:35 +0200

    trying to fix where display_mode doesnt exist

--- next-env.d.ts
+++ next-env.d.ts
@@ -8,10 +8,10 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      first_name: string;
-      last_name: string;
-      email: string;
-      display_mode: number;
+      first_name?: string;
+      last_name?: string;
+      email?: string;
+      display_mode?: number;
     };
   }
 }
