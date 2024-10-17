9252999fa02b07f9ea12d69c74ac9f110664270c
Levi Noppers <=>
2024-10-17 19:42:35 +0200

    reset file

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,15 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
-import NextAuth from "next-auth";
-
-declare module "next-auth" {
-  interface Session {
-    user: {
-      first_name?: string;
-      last_name?: string;
-      email?: string;
-      display_mode?: number;
-    };
-  }
-}
