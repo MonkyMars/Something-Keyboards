14667e4935fddfff0ad078441cd54fddfbf9e363
LeviNoppers <155177968+MonkyMars@users.noreply.github.com>
2024-10-17 13:20:35 +0000

    fixed error where session might be undefined

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -52,7 +52,7 @@ const UserEffect = () => {
 
   useEffect(() => {
     const root = document.documentElement;
-    if (session?.user.display_mode) {
+    if (session && session?.user.display_mode) {
       root.style.setProperty(
         "--Bg",
         session.user.display_mode === 1 ? "#272727" : "#e3e3e3"
